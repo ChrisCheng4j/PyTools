8dfef625af23999bfb3918f795579f8857ef3946
chrischeng <357213557@qq.com>
2016-03-15 20:39:20 +0800

    Add the java sample code to get channel name in android application.

---
+++ sample_channel.java
@@ -0,0 +1,48 @@
+package <your package>;
+
+import android.content.pm.ApplicationInfo;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ChannelConfig {
+
+    private static final String PREFIX = "channel";
+    private static final String EXPRESSION = "_";
+
+    public static String getChannel() {
+        ApplicationInfo info = <your application context>.getApplicationInfo();
+        String sourceDir = info.sourceDir;
+
+        ZipFile zipFile = null;
+        try {
+            zipFile = new ZipFile(sourceDir);
+            Enumeration entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = (ZipEntry) entries.nextElement();
+                String entryName = entry.getName();
+                if (entryName.startsWith(PREFIX)) {
+                    String[] split = entryName.split(EXPRESSION);
+                    if (split != null && split.length >= 2)
+                        return entryName.substring(split[0].length() + 1);
+                    else
+                        return "";
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (zipFile != null) {
+                try {
+                    zipFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return "";
+    }
+}
